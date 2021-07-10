d691f8edaf26b02cf1c7349aee0a79f98d265c17
desophos <1887450+desophos@users.noreply.github.com>
2021-07-10 07:51:46 -0500

    More error checking

--- scripts/stencyl2ldb.py
+++ scripts/stencyl2ldb.py
@@ -18,10 +18,22 @@ if __name__ == "__main__":
     db = plyvel.DB(str(args.ldb))
     key = db_key(args.idleon)
 
+    try:
+        assert db.get(key) is not None
+    except plyvel.CorruptionError as e:
+        raise IOError(f"Could not access key {key} in database at {args.ldb}") from e
+    except AssertionError as e:
+        raise IOError(
+            f"Key should already exist in database, but doesn't: {key}"
+        ) from e
+
     with open(args.file, "r", encoding="ascii") as file:
         val = file.read().strip()
 
-    db.put(key, b"\x01" + bytes(val, encoding="ascii"))
+    try:
+        db.put(key, b"\x01" + bytes(val, encoding="ascii"))
+    except plyvel.CorruptionError as e:
+        raise IOError(f"Failed to edit key {key} in database at {args.ldb}") from e
 
     db.close()
 
