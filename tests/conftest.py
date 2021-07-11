4d216591d73ba043fef5dd5502f56a67d6b379fb
desophos <1887450+desophos@users.noreply.github.com>
2021-07-10 20:13:07 -0500

    Move stencylsave to dedicated data directory

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,5 +3,5 @@ import pytest
 
 @pytest.fixture
 def stencylsave():
-    with open("tests/stencylsave.txt", "r") as f:
+    with open("tests/data/stencylsave.txt", "r") as f:
         return f.read()
