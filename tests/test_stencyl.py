74d048cda1d2289ca11cb92538810e7c2b0b7c22
desophos <1887450+desophos@users.noreply.github.com>
2021-07-11 01:05:13 -0500

    Fix slow difflib call in stencyl inversion test

--- tests/test_stencyl.py
+++ tests/test_stencyl.py
@@ -1,5 +1,13 @@
 from idleon_save_editor.stencyl import StencylDecoder, StencylEncoder
+from scripts.common import chunk
 
 
 def test_inversion(stencylsave):
-    assert stencylsave == StencylEncoder(StencylDecoder(stencylsave).result).result
+    decoded = StencylDecoder(stencylsave).result
+    encoded = StencylEncoder(decoded).result
+    # pytest uses difflib for equality assertions,
+    # which is extremely slow in this case:
+    # at least several minutes for 400k+ length strings.
+    # Splitting these strings into lines helps execution time
+    # and produces a more useful diff.
+    assert chunk(stencylsave, 50) == chunk(encoded, 50)
