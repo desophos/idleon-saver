94178dff670d5414f3a7a1ec88d7b9395d02512b
desophos <1887450+desophos@users.noreply.github.com>
2021-07-31 04:16:27 -0500

    Config data

--- idleon_saver/utility.py
+++ idleon_saver/utility.py
@@ -1,5 +1,14 @@
+import os
 from pathlib import Path
 from typing import List
+
+BUGREPORT_LINK = "https://github.com/desophos/idleon-saver/issues/new?assignees=desophos&labels=bug&template=bug_report.md&title="
+
+
+def user_dir():
+    path = Path(os.environ["APPDATA"]) / "IdleonSaver"
+    path.mkdir(exist_ok=True)
+    return path
 
 
 def chunk(s: str, chunk_size: int) -> List[str]:
