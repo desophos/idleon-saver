d881cc23acf2e7ec91d8abc1deeec89c9b408a43
desophos <1887450+desophos@users.noreply.github.com>
2021-12-31 01:07:53 -0600

    Retrieve player names and add them to save data

--- idleon_saver/scripts/inject.js
+++ idleon_saver/scripts/inject.js
@@ -2,8 +2,10 @@ async function topLevel() {
     const getFirebaseData = async () => {
         const fb = window.frames[0].firebase
         const uid = fb.auth().currentUser.uid
-        const docPromise = fb.firestore().doc("_data/" + uid).get()
-        return await docPromise.then(doc => doc.data())
+        const data = await fb.firestore().collection("_data").doc(uid).get().then(doc => doc.data())
+        const names = await fb.database().ref().child("_uid").child(uid).once("value").then(res => res.val())
+        data.PlayerNames = names
+        return data
     }
 
     // https://solutional.ee/blog/2020-11-19-Proper-Retry-in-JavaScript.html
