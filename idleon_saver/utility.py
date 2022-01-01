f84c9cbdaf5702b48d00ad28484688f885efb673
desophos <1887450+desophos@users.noreply.github.com>
2022-01-01 04:30:48 -0600

    Refactor CLI arg handling

--- idleon_saver/utility.py
+++ idleon_saver/utility.py
@@ -1,10 +1,130 @@
+import logging
 import os
+import sys
 import time
+from argparse import Action, ArgumentParser
+from enum import Enum
 from pathlib import Path
 from typing import Any, Callable, Iterable, List
 
+from idleon_saver.scripts.export import Formats, Sources
+
 ROOT_DIR = Path(__file__).resolve().parent.parent
 BUGREPORT_LINK = "https://github.com/desophos/idleon-saver/issues/new?assignees=desophos&labels=bug&template=bug_report.md&title="
+
+
+class Args(Enum):
+    IDLEON = "idleon"
+    LDB = "ldb"
+    WORKDIR = "workdir"
+    INFILE = "infile"
+    OUTFILE = "outfile"
+    SOURCE = "source"
+    TO = "to"
+
+
+class IdleonAction(Action):
+    def __call__(self, parser, namespace, value, option_string=None):
+        # In case someone passes the exe path instead of the install dir.
+        if value.name == "LegendsOfIdleon.exe":
+            value = value.parent
+        setattr(namespace, self.dest, value)
+
+
+class LdbAction(Action):
+    def __call__(self, parser, namespace, value, option_string=None):
+        # Only check ldb path.
+        # Idleon path is only used for the db key, so it doesn't have to exist.
+        # (Allows running from VMs.)
+        if not (value.exists() and value.is_dir()):
+            raise IOError(f"Invalid leveldb path: {value}")
+        setattr(namespace, self.dest, value)
+
+
+class WorkdirAction(Action):
+    def __call__(self, parser, namespace, value, option_string=None):
+        value.mkdir(exist_ok=True)
+        setattr(namespace, self.dest, value)
+
+
+class SourceAction(Action):
+    def __call__(self, parser, namespace, value, option_string=None):
+        setattr(namespace, self.dest, Sources(value))
+
+
+class ToAction(Action):
+    def __call__(self, parser, namespace, value, option_string=None):
+        setattr(namespace, self.dest, Formats(value))
+
+
+arg_adders: dict[Args, Callable[[ArgumentParser], Any]] = {
+    Args.IDLEON: lambda parser: parser.add_argument(
+        "-n",
+        "--idleon",
+        type=Path,
+        default="C:/Program Files (x86)/Steam/steamapps/common/Legends of Idleon",
+        help="your Legends of Idleon install path",
+        action=IdleonAction,
+    ),
+    Args.LDB: lambda parser: parser.add_argument(
+        "-l",
+        "--ldb",
+        type=resolved_path,
+        default="~/dev/leveldb",
+        help="path to the leveldb to work with",
+        action=LdbAction,
+    ),
+    Args.WORKDIR: lambda parser: parser.add_argument(
+        "-w",
+        "--workdir",
+        type=resolved_path,
+        default=ROOT_DIR / "work",
+        help="path to the working directory where files will be created",
+        action=WorkdirAction,
+    ),
+    Args.INFILE: lambda parser: parser.add_argument(
+        "-i",
+        "--infile",
+        default="",
+        help="name of the input file; default varies by script",
+    ),
+    Args.OUTFILE: lambda parser: parser.add_argument(
+        "-o",
+        "--outfile",
+        default="",
+        help="name of the output file; default varies by script",
+    ),
+    Args.SOURCE: lambda parser: parser.add_argument(
+        "-s",
+        "--source",
+        choices=[member.value for member in Sources],
+        default=Sources.FIREBASE.value,
+        help="source of save data",
+        action=SourceAction,
+    ),
+    Args.TO: lambda parser: parser.add_argument(
+        "-t",
+        "--to",
+        choices=[member.value for member in Formats],
+        default=Formats.IC.value,
+        help="format to parse save data into",
+        action=ToAction,
+    ),
+}
+
+
+def get_args(to_add: list[Args]):
+    # Redirect logs to stdout for CLI scripts.
+    root = logging.getLogger()
+    root.setLevel(logging.INFO)
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.INFO)
+    root.addHandler(handler)
+
+    parser = ArgumentParser()
+    for arg in to_add:
+        arg_adders[arg](parser)
+    return parser.parse_args()
 
 
 def user_dir() -> Path:
